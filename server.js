231d1a4ac8bffe71e218e9859d60bbacee8c4acd
Vinod Kumar KP <vinod.kunum-purath@capgemini.com>
2026-05-04 14:16:58 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = 8082;
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'demo-dist')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
